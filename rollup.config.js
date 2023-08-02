9c22755499109b7f2c721e1adb96e7dfe9728965
tonyzzp <tonyzzp@qq.com>
2023-08-02 20:05:06 +0800

    done

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "rollup"
+import rollupPluginTypescript from "@rollup/plugin-typescript"
+import rollupPluginNodeResolve from "@rollup/plugin-node-resolve"
+import rollupComonjs from "@rollup/plugin-commonjs"
+import rollupJson from "@rollup/plugin-json"
+
+export default defineConfig({
+    input: "main.ts",
+    output: {
+        file: "dist/main.js",
+        format: "es"
+    },
+    plugins: [
+        rollupPluginTypescript(),
+        rollupPluginNodeResolve(),
+        rollupComonjs(),
+        rollupJson(),
+    ],
+})
